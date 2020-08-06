dc3d0ab897836df841a57777828a427f43017651
atul.linux01 <atul.linux01@gmail.com>
2020-08-06 20:29:31 +0530

    latest code of components  user table -06-08-2020

---
+++ src/app/pages/users/data.ts
@@ -0,0 +1,22 @@
+const tableData = [
+  {
+    name: 'Alex',
+    role: 'Org Admin',
+  },{
+    name: 'Bobby',
+    role: 'Developer',
+  },
+  {
+    name: 'Bobby',
+    role: 'Developer',
+  },
+  {
+    name: 'Bobby',
+    role: 'Developer',
+  },{
+    name: 'Bobby',
+    role: 'Developer',
+  }
+];
+
+export { tableData };
---
+++ src/app/pages/users/users.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+
+import { Observable } from 'rxjs';
+
+import { Table } from './users.model';
+
+import { tableData } from './data';
+
+import { UsersService } from './users.service';
+import { UsersSortableDirective, SortEvent } from './users-sortable.directive';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+  providers: [UsersService, DecimalPipe]
+})
+
+/**
+ * Advanced table component
+ */
+export class UsersComponent implements OnInit {
+  // bread crum data
+  breadCrumbItems: Array<{}>;
+
+  // Table data
+  tableData: Table[];
+
+  tables$: Observable<Table[]>;
+  total$: Observable<number>;
+
+  @ViewChildren(UsersSortableDirective) headers: QueryList<UsersSortableDirective>;
+
+  constructor(public service: UsersService) {
+    this.tables$ = service.tables$;
+    this.total$ = service.total$;
+  }
+  ngOnInit() {
+
+    //this.breadCrumbItems = [{ label: 'Tables' }, { label: 'Cluster', active: false }];
+    this.breadCrumbItems =[];
+
+    /**
+     * fetch data
+     */
+    this._fetchData();
+  }
+
+
+  /**
+   * fetches the table value
+   */
+  _fetchData() {
+    this.tableData = tableData;
+  }
+
+  /**
+   * Sort table data
+   * @param param0 sort the column
+   *
+   */
+  onSort({ column, direction }: SortEvent) {
+    // resetting other headers
+    this.headers.forEach(header => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+    this.service.sortColumn = column;
+    this.service.sortDirection = direction;
+  }
+}
+
